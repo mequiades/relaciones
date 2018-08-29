d39d99b1fcbf12e6a1d76b3275dfb591520c61a8
Estudiante <Estudiante@10.203.1.68>
2018-08-29 08:07:13 -0500

    Relaciones

---
+++ src/relaciones/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package relaciones;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Main {
+    public static void main (String [] args) {
+        Fecha fNacimiento = new Fecha (10, 10, 1980);
+        Fecha FIngreso = new Fecha (29,8, 2018);
+        Empleado empleado = New Empleado ("V", "G",)
+    }
+}
